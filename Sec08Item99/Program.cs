fca77ea80eb971e2471a74f2217a3f643bf44a99
Edson Iwata <eiwata@hotmail.com>
2020-02-27 18:29:15 -0300

    projeto iniciado mostrando Hello World

---
+++ Sec08Item99/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sec08Item99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
