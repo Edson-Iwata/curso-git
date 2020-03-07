66273f599b92b274aee2b3cd21a3b40cd2cd96fc
Edson Iwata <eiwata2020@gmail.com>
2020-03-07 13:20:35 -0300

    Acrescentada mensagem de despedida

--- Sec08Item99/Program.cs
+++ Sec08Item99/Program.cs
@@ -9,6 +9,7 @@ namespace Sec08Item99
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje ?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
 
             Console.ReadLine();
         }
